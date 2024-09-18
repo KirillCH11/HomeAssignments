3e14aae9fdaf4bc184ca2abbc371a4c8dfcab60b
Kirill Churkin <st129364@student.spbu.ru>
2024-09-18 23:03:58 +0300

    Add a header

---
+++ hello.h
@@ -0,0 +1,17 @@
+/* Churkin Kirill st129364@student.spbu.ru
+	first test project
+*/
+
+#ifndef HELLO_h
+#define HELLO_h
+
+#include <string>
+
+namespace f_p
+(
+	void SayHello();
+	void SayHello(const std::string &name);
+	
+)
+
+#endif
