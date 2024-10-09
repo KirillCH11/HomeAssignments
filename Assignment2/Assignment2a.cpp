c79e6885dbc877e6ecc879af8c0a296359d7ee4e
Kirill Churkin <st129364@student.spbu.ru>
2024-10-09 17:23:19 +0300

    Did the task a

---
+++ Assignment2/Assignment2a.cpp
@@ -0,0 +1,32 @@
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+
+
+int  main()
+{
+    std:: ifstream file("test.txt", std::ios_base::binary);
+    int size = std::filesystem::file_size("test.txt");   
+    int *lst = new int [size];
+    file.read((char*)lst, size);
+    file.close();
+
+    //for(int i = 0; i < size; ++i)
+    //    std::cout << lst[i] << std::endl;
+
+    int x;
+    for(int i = 0; i < size - i - 1; ++i)
+    {
+        x = lst[i];
+        lst[i] = lst[size - i - 1];
+        lst[size - i - 1] = x;
+    }
+
+    //for(int i = 0; i < size; ++i)
+    //    std::cout << lst[i] << std::endl;
+
+    std::ofstream file_out("test_out.txt", std::ios_base::binary);
+    file_out.write((char *)lst, sizeof(size));
+    file_out.close();
+    delete[] lst;
+}
