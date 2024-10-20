8b55ce4519df6f2cd86534bd855cc8eec1bc1409
Kirill Churkin <st129364@student.spbu.ru>
2024-10-20 18:10:30 +0300

    Broke the program into functions

--- Assignment2/Assignment2b/Assignment2b.cpp
+++ Assignment2/Assignment2b/Assignment2b.cpp
@@ -6,21 +6,28 @@
 #include <string>
 
 
+float calculator(std::string st);
+void work();
+
+
 int main()
 {
-    std::string st;
-    std::cout << "Input your polish notation: ";
-    std::cin >> st;
+    work();
+}
 
-    float *lst = new float [st.length()];
+
+float calculator(std::string st)
+{
+	float *lst = new float [st.length()];
     int total = 0;
     int res = 0;
+    
     for(int i = 0; i < st.length(); ++i)
     {
         if (st[i] != '+' && st[i] != '-' && st[i] != '*' && st[i] != '/')
         {   
-	    lst[i] = st[i] - '0';
-	}
+	    	lst[i] = st[i] - '0';
+		}
         else if (st[i] == '+')
         {
             lst[i] = lst[i - 2] + lst[i - 1];
@@ -38,8 +45,21 @@ int main()
             lst[i] = lst[i - 2] / lst[i - 1];
         }
     }
-    std::cout << "Your result is: " << lst[st.length() - 1] << std::endl;
-    //for(int i = 0; i < st.length(); ++i)
-    //    std::cout << lst[i] << std::endl;
+
+    return lst[st.length() - 1];
     delete[] lst;
 }
+
+
+void work()
+{
+	std::string st;
+    std::cout << "Input your polish notation: ";
+    std::cin >> st;
+	
+	float rez;
+	rez = calculator(st);
+   
+    std::cout << "Your result is: " << rez << std::endl;
+}
+
