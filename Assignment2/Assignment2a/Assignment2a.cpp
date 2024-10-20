81d4bb176c502508eff35cf1e8318622b56aba69
Kirill Churkin <st129364@student.spbu.ru>
2024-10-20 17:41:52 +0300

    Broke the program into functions

--- Assignment2/Assignment2a/Assignment2a.cpp
+++ Assignment2/Assignment2a/Assignment2a.cpp
@@ -8,27 +8,40 @@
 #include <fstream>
 
 
-int  main()
+void reverse(char *lst);
+void work();
+
+
+int main()
 {
-    std:: ifstream file("test.txt", std::ios_base::binary);
-    int size = std::filesystem::file_size("test.txt");   
-    int *lst = new int [size];
-    file.read((char*)lst, size);
-    file.close();
+    work();
+}
 
-    //for(int i = 0; i < size; ++i)
-    //    std::cout << lst[i] << std::endl;
 
-    int x;
-    for(int i = 0; i < size - i - 1; ++i)
+void reverse(char *lst)
+{
+	int x;
+	int size = sizeof(lst);
+    for(int i = 0; i < size - i - 1; i++)
     {
         x = lst[i];
         lst[i] = lst[size - i - 1];
         lst[size - i - 1] = x;
     }
+}
 
-    //for(int i = 0; i < size; ++i)
-    //    std::cout << lst[i] << std::endl;
+
+void work()
+{
+	std:: ifstream file("test.txt", std::ios_base::binary);
+    int size = std::filesystem::file_size("test.txt");   
+    
+    char* lst = new char[size];
+    
+    file.read((char*)lst, size);
+    file.close();
+
+    reverse(lst);
 
     std::ofstream file_out("test_out.txt", std::ios_base::binary);
     file_out.write((char *)lst, sizeof(size));
